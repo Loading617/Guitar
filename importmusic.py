ed4f08b07201d91bb0141289af639af2ba437a40
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-04-13 19:36:46 -0400

    removed settings & added importmusic.py

---
+++ importmusic.py
@@ -0,0 +1,3 @@
+# Import Music button
+        self.import_button ttk.Button(self.control_frame, text = "Import Music", command=self.import_music)
+        self.import_button.grid(row =6, column=0, padx=10, pady=10)
