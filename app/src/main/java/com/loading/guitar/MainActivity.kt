3a6bdaa81418b992a60e600bc97373240607ec33
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-08-06 03:09:19 -0400

    Update MainActivity.kt

--- app/src/main/java/com/loading/guitar/MainActivity.kt
+++ app/src/main/java/com/loading/guitar/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.loading.guitar
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import com.loading.guitar.databinding.ActivityMainBinding
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
