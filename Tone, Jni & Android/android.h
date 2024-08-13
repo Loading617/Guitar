b30d7e8ba5c7fe775abbb4482378228204b40e83
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-08-13 03:27:11 -0400

    Delete Tone, Jni & Android directory

--- Tone, Jni & Android/android.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by jahim on 8/12/2024.
-//
-
-#ifndef GUITAR_ANDROID_H
-#define GUITAR_ANDROID_H
-
-#endif //GUITAR_ANDROID_H
--- Tone, Jni & Android/jni.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by jahim on 8/12/2024.
-//
-
-#ifndef GUITAR_JNI_H
-#define GUITAR_JNI_H
-
-#endif //GUITAR_JNI_H
