af11bae723f579e6ceed08e3ec1303e29433bcb4
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-07 23:18:11 -0400

    Add files via upload

---
+++ app/src/main/java/com/loading/guitar/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.loading.guitar.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
